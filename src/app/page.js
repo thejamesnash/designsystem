8f241835829ca7a9821ab84879b3b6e6c06344bb
James Nash <jmnxvii@gmail.com>
2026-02-26 10:22:15 +0000

    Update page.js

--- src/app/page.js
+++ src/app/page.js
@@ -3,7 +3,7 @@ import styles from "./page.module.css";
 export default function Home() {
   return (
     <div>
-        <h1>Tets</h1>
+        <h1>This is where the app lives</h1>
     </div>
   );
 }
